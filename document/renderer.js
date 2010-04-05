973956162745d13f0b5c9163f55a3b0de68d810c
Pablo Kang <pablokang@pablo.sharegrove.com>
2010-04-05 16:33:44 -0700

    new -> create

--- document/renderer.js
+++ document/renderer.js
@@ -1,4 +1,4 @@
 var el = msjs.publish($(<div/>));
-el.append(msjs.require("chaise.document.new.renderer"))
+el.append(msjs.require("chaise.document.create.renderer"))
   .append(msjs.require("chaise.document.list.renderer"))
   .append(msjs.require("chaise.document.detail.renderer"));
