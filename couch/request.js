d331adea1accd63a9cde83a1bfd1ce9b5e19a7d2
Pablo Kang <pablokang@pablo.sharegrove.com>
2010-04-07 11:01:31 -0700

    Handle java exceptions

--- couch/request.js
+++ couch/request.js
@@ -2,19 +2,22 @@ var jsonRequest = msjs.require("msjs.jsonrequest");
 var request = msjs.publish({});
 request.packMe = false;
 var context = msjs.require("java.org.mozilla.javascript.Context");
-var javaToJS = function(o) {
-    if (o instanceof java.lang.Object) {
-        o = o.toString();
+var handleException = function(e) {
+    if (e.javaException) {
+        var writer = new Packages.java.io.StringWriter();
+        e.javaException.printStackTrace(new Packages.java.io.PrintWriter(writer, true));
+        writer.flush();
+        e = writer.toString();
     }
-    return o;
-};
+    return e;
+}
 request.get = function(url) {
     var response = {status: -1, result: null, url: url};
     try {
         response = jsonRequest.get(url);
         response.url = url;
    } catch (e) {
-        msjs.log("[warn] couch get:", javaToJS(e));
+        msjs.log("[warn] couch get: " + handleException(e));
    }
     return response;
 };
@@ -24,7 +27,7 @@ request.del = function(url) {
         response = jsonRequest.del(url);
         response.url = url;
    } catch (e) {
-        msjs.log("[warn] couch del:", javaToJS(e));
+        msjs.log("[warn] couch del: " + handleException(e));
    }
     return response;
 };
@@ -34,7 +37,7 @@ request.post = function(url, content, mimeType) {
         response = jsonRequest.get(url, content, mimeType);
         response.url = url;
    } catch (e) {
-        msjs.log("[warn] couch post:", javaToJS(e));
+        msjs.log("[warn] couch post: " + handleException(e));
    }
     return response;
 };
@@ -44,7 +47,7 @@ request.put = function(url, content, mimeType) {
         response = jsonRequest.put(url, content, mimeType);
         response.url = url;
    } catch (e) {
-        msjs.log("[warn] couch put:", javaToJS(e));
+        msjs.log("[warn] couch put: " + handleException(e));
    }
     return response;
 };
